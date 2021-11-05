a41ef6fd22c8dc009fff2881abb0e307e03d9dd5
Amulya Ojha <amulyaojha2@gmail.com>
2021-11-05 13:49:51 +0530

    added props

--- src/components/News.js
+++ src/components/News.js
@@ -4,16 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                Hi I am the News Component.
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
-                <NewsItem/>
+            <div className="container my-3">
+                <h2>NewsMonkey - Top Headlines</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc"/>
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc"/>
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc"/>
+                    </div>
+                </div>
             </div>
         )
     }
