a65dbca5bf059a61f081306c50ba02151fdd2435
Amulya Ojha <amulyaojha2@gmail.com>
2021-11-05 12:56:59 +0530

    added class components

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        return (
+            <div>
+                Hey I am the NewsItem component.
+            </div>
+        )
+    }
+}
+
+export default NewsItem
